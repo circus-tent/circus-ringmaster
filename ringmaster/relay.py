b8b6491c1b9b5d32274f18db870e376fb9d98593
Rémy HUBSCHER <hubscher.remy@gmail.com>
2013-07-15 15:50:45 +0200

    PoC of the circus-ringmaster on the status command

--- ringmaster/relay.py
+++ ringmaster/relay.py
@@ -1,10 +1,12 @@
+import json
 import random
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 import zmq
 from zmq.eventloop import ioloop
 from tornado import gen
 
+from circus import logger
 from circus.async.client import AsyncClient
 from circus.stats.asyncclient import AsyncStatsConsumer
 from circus.util import DEFAULT_ENDPOINT_DEALER
@@ -73,14 +75,60 @@ class Relay(BaseController):
         self.last_stats[fqdn] = stats
 
     @gen.coroutine
+    def handle_message(self, raw_msg):
+        cid, msg = raw_msg
+        msg = msg.strip()
+
+        if not msg:
+            self.send_response(cid, msg, "error: empty command")
+        else:
+            logger.debug("got message %s", msg)
+            try:
+                yield gen.Task(self.dispatch, cid, msg)
+            except Exception as e:
+                print str(e)
+                self.send_error(cid, msg, 'Handle Error: %s' % e)
+
+    @gen.coroutine
+    def dispatch(self, cid, msg):
+        try:
+            json_msg = json.loads(msg)
+        except ValueError:
+            raise gen.Return(self.send_error(cid, msg, "json invalid",
+                                             errno=errors.INVALID_JSON))
+
+        cmd_name = json_msg.get('command')
+        properties = json_msg.get('properties', {})
+        cast = json_msg.get('msg_type') == "cast"
+
+        resp = yield gen.Task(self.execute_command, cmd_name, properties, cid, msg, cast)
+
+        if resp is None:
+            resp = ok()
+
+        if not isinstance(resp, (dict, list,)):
+            msg = "msg %r tried to send a non-dict: %s" % (msg, str(resp))
+            logger.error("msg %r tried to send a non-dict: %s", msg, str(resp))
+            raise gen.Return(self.send_error(cid, msg, "server error", cast=cast,
+                                             errno=errors.BAD_MSG_DATA_ERROR))
+
+        if isinstance(resp, list):
+            resp = {"results": resp}
+
+        self.send_ok(cid, msg, resp, cast=cast)
+
+    @gen.coroutine
     def execute_command(self, cmd_name, properties, cid, msg, cast):
         targets = [self._get_client(f) for f in self.nodes]
         if cmd_name in ('status',):
             results = yield [gen.Task(t.send_message, cmd_name, **properties)
                              for t in targets]
-            print dict(zip(self.nodes, results))
-            # self.send_response(cid, msg, dict(zip(self.nodes, results)))
-            raise gen.Return(self.send_ok(cid, msg))
+            
+            answer = OrderedDict()
+            for i, node in enumerate(self.nodes):
+                for key, value in results[i]['statuses'].iteritems():
+                    answer['%s:%s' % (node, key)] = value
+            raise gen.Return(dict(statuses=answer))
 
     def _get_client(self, fqdn):
         # XXX fallback if we don't manage to establish a connection with the
@@ -91,7 +139,6 @@ class Relay(BaseController):
                                               endpoint=endpoint)
         return self._clients[fqdn]
 
-    send_response = gen.coroutine(BaseController.send_response)
 
 if __name__ == '__main__':
 
